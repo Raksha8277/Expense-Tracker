ff7ddd6edee4d106dabae532524cb77ed9f50f42
Raksha8277 <nbraksha@gmail.com>
2026-04-19 17:56:49 +0530

    Update Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -29,6 +29,8 @@ export default function Navbar() {
                 : "bg-red-100 text-red-600 hover:bg-red-200"
             }`}
           >
+
+            Expense History
            
           </button>
 
@@ -40,7 +42,7 @@ export default function Navbar() {
                 : "bg-green-100 text-green-600 hover:bg-green-200"
             }`}
           >
-            
+            Income History
           </button>
 
         </div>
