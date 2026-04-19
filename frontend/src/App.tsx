7e29103ca93220419ed85970234974360995ac49
Raksha8277 <nbraksha@gmail.com>
2026-04-19 14:09:29 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import ExpensePage from "./pages/ExpensePage";
+import IncomePage from "./pages/IncomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-      </Routes> 
+        <Route path="/expenses" element={<ExpensePage />} />
+        <Route path="/income" element={<IncomePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
