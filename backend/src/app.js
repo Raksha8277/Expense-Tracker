4fd9ce6f75609fe5b469b8789c2030c35998b988
Raksha8277 <nbraksha@gmail.com>
2026-04-19 21:09:00 +0530

    Update backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => {
+  res.send("Expense Tracker Backend is Running");
+});
+
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/expenses", require("./routes/expenseRoutes"));
 
