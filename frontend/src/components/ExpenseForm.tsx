238679d13d342fd0261afa6450ceee931405ad42
Raksha8277 <nbraksha@gmail.com>
2026-04-19 12:40:34 +0530

    Update Expense form

--- frontend/src/components/ExpenseForm.tsx
+++ frontend/src/components/ExpenseForm.tsx
@@ -3,43 +3,53 @@ import { useState } from "react";
 export default function ExpenseForm({ onAdd }: any) {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState("");
+  const [type, setType] = useState("expense"); // ✅ NEW
 
   const handleSubmit = () => {
     if (!title || !amount) return alert("Fill all fields");
 
-    onAdd({ title, amount });
+    onAdd({ title, amount, type }); // ✅ send type also
     setTitle("");
     setAmount("");
+    setType("expense");
   };
 
   return (
     <div className="mt-6 p-5 bg-white/40 backdrop-blur-lg rounded-2xl shadow-lg border border-white/30">
 
-      
       <h2 className="text-xl font-semibold text-indigo-700 mb-4">
-        ➕ Add New Expense
+        Add Transaction
       </h2>
 
-      
       <div className="flex flex-col sm:flex-row gap-3">
 
         <input
-          className="flex-1 p-3 rounded-xl bg-white/80 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition"
-          placeholder="Enter title (e.g. Food, Travel)"
+          className="flex-1 p-3 rounded-xl bg-white/80"
+          placeholder="Title"
           value={title}
           onChange={e => setTitle(e.target.value)}
         />
 
         <input
-          className="flex-1 p-3 rounded-xl bg-white/80 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition"
+          className="flex-1 p-3 rounded-xl bg-white/80"
           placeholder="Amount (₹)"
           value={amount}
           onChange={e => setAmount(e.target.value)}
         />
 
+
+        <select
+          className="p-3 rounded-xl bg-white/80"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="expense">Expense</option>
+          <option value="income">Income</option>
+        </select>
+
         <button
           onClick={handleSubmit}
-          className="px-6 py-3 rounded-xl bg-gradient-to-r from-indigo-500 to-purple-600 text-white font-semibold shadow-md hover:scale-105 hover:shadow-xl transition duration-300"
+          className="px-6 py-3 rounded-xl bg-indigo-500 text-white"
         >
           Add
         </button>
